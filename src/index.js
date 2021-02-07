c026e392095b58dc736208977b170a50ec615df0
Pius Wong <pius@pioslabs.com>
2021-02-06 18:30:00 -0600

    Initialize project w/ basic imports and Hello World

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// the name of this file is important!
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// Import the React and ReactDOM libraries
+import React from 'react'; // good to follow convention of naming variable after library
+import reactDom from 'react-dom';
+import ReactDOM from 'react-dom';
+
+// require vs. import:  import for ES2015 modules, require is for CommonJS modules...
+
+// Create a React component
+//      --> a function or class that produces HTML (via JSX) and handles user feedback (event handlers)
+const App = () => {
+    return <div>Hello world!</div>;
+};
+
+reactDom.render(
+    <App />,
+    document.querySelector('#root') // references HTML doc
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Take the React component, and show it on the screen
+
+
+
+// these lines recommended for Create-React-App to enable "hot reloading"
+if (module.hot) {
+    module.hot.accept();
+}
