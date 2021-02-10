76bfb01e19b599e94522dd1c292926d8178de4e8
Pius Wong <pius@pioslabs.com>
2021-02-09 18:20:36 -0600

    Reference JS variable function in button

--- src/index.js
+++ src/index.js
@@ -7,23 +7,27 @@ import ReactDOM from 'react-dom';
 
 // require vs. import:  import for ES2015 modules, require is for CommonJS modules...
 
-// styles
-// in JSX (like in return) use "" for strings, but for non-JSX props, use '' by general convention
-const style1 = { 
-    backgroundColor: 'green', 
-    color: 'white' 
-};
+function getButtonText(){
+    return 'Click on me!';
+}
 
 // Create a React component
 //      --> a function or class that produces HTML (via JSX) and handles user feedback (event handlers)
 const App = () => {
+    // styles
+    // in JSX (like in return) use "" for strings, but for non-JSX props, use '' by general convention
+    const style1 = { 
+        backgroundColor: 'green', 
+        color: 'white' 
+    };
+
     // after the return, this is JSX; converted to normal JS via babel (node module)
     // JSX should use className instead of class to avoid conflict with JS 'class'
     return (
         <div>
             <label className="label" for="name">Enter name:</label> 
             <input id="name" type="text" />
-            <button style={ style1 }>Submit</button>
+            <button style={ style1 }>{ getButtonText() }</button>
         </div>
     );
 };
