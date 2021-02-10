c5c09d7a8d76bbcbcbd8bec1f103ec6ec7f8d951
Pius Wong <pius@pioslabs.com>
2021-02-09 18:13:13 -0600

    Style input elements

--- src/index.js
+++ src/index.js
@@ -7,10 +7,20 @@ import ReactDOM from 'react-dom';
 
 // require vs. import:  import for ES2015 modules, require is for CommonJS modules...
 
+// styles
+const style1 = { backgroundColor: 'green', color: 'white' };
+
 // Create a React component
 //      --> a function or class that produces HTML (via JSX) and handles user feedback (event handlers)
 const App = () => {
-    return <div>Hello world!</div>;
+    // after the return, this is JSX; converted to normal JS via babel (node module)
+    return (
+        <div>
+            <label class="label" for="name">Enter name:</label>
+            <input id="name" type="text" />
+            <button style={ style1 }>Submit</button>
+        </div>
+    );
 };
 
 reactDom.render(
